64527b92afa5b97b703cab0e70c1155121aef9fc
f3nrir197x <130315497+f3nrir197x@users.noreply.github.com>
2023-05-22 19:12:54 -0400

    Update instagram.php
    Updated considering recent version of Instagram API

--- instagram.php
+++ instagram.php
@@ -4,7 +4,6 @@ namespace Grav\Plugin;
 use Grav\Common\Plugin;
 use Grav\Common\Data\Data;
 use Grav\Common\Page\Page;
-use Grav\Common\GPM\Response;
 
 class InstagramPlugin extends Plugin
 {
@@ -80,26 +79,50 @@ class InstagramPlugin extends Plugin
         // Init the cache engine
         $this->cache = phpFastCache("files", $cache_config);
 
-        // Generate API url
-        $url = 'https://api.instagram.com/v1/users/self/media/recent/?access_token=' . $config->get('feed_parameters.access_token').'&count=' . $config->get('feed_parameters.count');
+        // Set access token
+        $access_token = $config->get('feed_parameters.access_token');
+        
+        // Set number of items to retrieve
+        $count = $config->get('feed_parameters.count');
 
+        // Set fields to retrieve
+        $fields = "id,caption,media_type,media_url,permalink,thumbnail_url,timestamp,username";
+        
+        // Instagram API url
+        $url = "https://graph.instagram.com/me/media?fields={$fields}&access_token={$access_token}&limit={$count}";
+        
         // Get the cached results if available
         $results = $this->cache->get($url);
 
         // Get the results from the live API, cached version not found
         if ($results === null) {
-            $results = Response::get($url);
-
+            // Get the results from the live API
+            $response = @file_get_contents($url);
+            $results = json_decode($response, true, 512, JSON_BIGINT_AS_STRING);
+            
             // Cache the results
             $this->cache->set($url, $results, InstagramPlugin::HOUR_IN_SECONDS * $config->get('feed_parameters.cache_time')); // Convert hours to seconds
         }
 
-        $this->parseResponse($results);
+        $feed = [];
+        foreach($results["data"] as $post){
+            $item = [];
+            
+            $item['link'] = $post['permalink'];
+            $item['image'] = $post['media_url'];
+            $item['thumbnail'] = $post['thumbnail_url'];
+            $item['username'] = $post['username'];
+            $item['caption'] = $post['caption'];
+            $item['type'] = $post['media_type'];
+            $item['timestamp'] = $post['timestamp'];
+            
+            $feed[] = $item;
+        }
 
         $this->template_vars = [
             'user_id'   => $config->get('feed_parameters.user_id'),
             'client_id' => $config->get('feed_parameters.client_id'),
-            'feed'      => $this->feeds,
+            'feed'      => $feed,
             'count'     => $config->get('feed_parameters.count'),
             'params'    => $params
         ];
@@ -107,38 +130,5 @@ class InstagramPlugin extends Plugin
         $output = $this->grav['twig']->twig()->render($this->template_html, $this->template_vars);
 
         return $output;
-    }
-
-    private function addFeed($result) {
-        foreach ($result as $key => $val) {
-            if (!isset($this->feeds[$key])) {
-                $this->feeds[$key] = $val;
-            }
-        }
-        krsort($this->feeds);
-    }
-
-    private function parseResponse($json) {
-        $r = array();
-        $content = json_decode($json, true);
-        if (count($content['data'])) {
-            foreach ($content['data'] as $key => $val) {
-                $created_at = $val['created_time'];
-                $r[$created_at]['time'] = $created_at;
-                $r[$created_at]['text'] = $val['caption']['text'];
-                $r[$created_at]['image'] = $val['images']['standard_resolution']['url'];
-                $r[$created_at]['image_width'] = $val['images']['standard_resolution']['width'];
-                $r[$created_at]['thumb'] = $val['images']['low_resolution']['url'];
-                $r[$created_at]['thumb_width'] = $val['images']['low_resolution']['width'];
-                $r[$created_at]['micro'] = $val['images']['thumbnail']['url'];
-                $r[$created_at]['micro_width'] = $val['images']['thumbnail']['width'];
-                $r[$created_at]['user'] = $val['user']['full_name'];
-                $r[$created_at]['link'] = $val['link'];
-                $r[$created_at]['comments'] = $val['comments']['count'];
-                $r[$created_at]['likes'] = $val['likes']['count'];
-                $r[$created_at]['type'] = $val['type'];
-            }
-            $this->addFeed($r);
-        }
     }
 }
